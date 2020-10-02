3910c24cfc81fae3779b2bf07514f28336b32311
emiliocm9 <emilio.contreras97@gmail.com>
2020-10-02 17:20:39 -0500

    docs: create projects class constructor

---
+++ src/projectconstructor.js
@@ -0,0 +1,15 @@
+myProjects = [];
+
+class Project {
+  constructor(name, description) {
+    this.name = name;
+    this.description = description;
+    this.tasks = []
+  }
+}
+
+const displayProjects = (projects) => {
+  for (let project in myProjects) {
+    project
+  }
+}
--- src/taskconstructor.js
+++ src/taskconstructor.js
@@ -1,5 +1,6 @@
 const myList = [];
 const myContainer = document.querySelector('.todo-container');
+const undoneContainer = document.querySelector('.todo-container-no');
 
 class Task {
   constructor(title, description, date, priority, completion) {
@@ -11,12 +12,41 @@ class Task {
   }
 }
 
-Task.prototype.sayHi = function() {
-  console.log('Hi my name is ' + this.title);
+const deleteTask = (myCard, listIndex) => {
+  myCard.querySelector('#delete').addEventListener('click', () => {
+    const element = document.querySelector(`[data-index="${listIndex}"]`);
+    myContainer.removeChild(element);
+    delete myList(listIndex);
+  });
+}
+
+const eventCheckList = (checkList, card, task) => {
+  checkList.addEventListener('change', () => {
+    if (task.completion) {
+      task.completion = false;
+      toggleCheckBox(checkList, card, task);
+    } else {
+      task.completion = true;
+      toggleCheckBox(checkList, card, task);
+    }
+  });
+}
+
+
+const toggleCheckBox = (checkList, card, task) => {
+  if (task.completion) {
+    checkList.checked = true;
+    checkCompletion = (task, card);
+    myContainer.appendChild(card);
+  } else {
+    checkList.checked = false;
+    checkCompletion = (task, card);
+    undoneContainer.appendChild(card);
+  }
 }
 
 const listDisplay = (task) => {
-  const listIndex = myLibrary.indexOf(task);
+  const listIndex = myList.indexOf(task);
 
   const myCard = document.createElement('div');
   myCard.className = 'card mt-2';
@@ -26,32 +56,42 @@ const listDisplay = (task) => {
       <h5 class="card-title">${task.title}</h5>
       <h6 class="card-subtitle mb-2 text-muted">${task.date}</h6>
       <p class="card-text">${task.description}</p>
-      <label for="completion">${task.completion}</label>
-      <a href="#" class="card-link">Card link</a>
-      <a href="#" class="card-link">Another link</a>
+      <p class="card-text text-uppercase">${task.priority} importance</p>
+      <div class="d-flex w-100 justify-content-between">
+        <input type="checkbox" class="read-check ml-2 mb-3"/>
+        <a href="#" id="delete" class="card-link text-danger text-uppercase">Delete</a>
+      </div>
     </div>`;
 
-  myContainer.appendChild(myCard);
-}
+  const checkList = myCard.querySelector('.read-check');
 
+  deleteTask(myCard, listIndex);
+  toggleCheckBox(checkList, myCard, task);
+  eventCheckList(checkList, myCard, task);
+}
 
 const addTaskToList = (title, description, date, priority, completion) => {
   const newTask = new Task(title, description, date, priority, completion);
   myList.push(newTask);
   listDisplay(newTask);
+  console.log(myList);
 }
 
-const uploadBook = () => {
+const uploadTask = () => {
   const savebutton = document.querySelector('#add-button');
 
   savebutton.addEventListener('click', () => {
-    const title = document.querySelector('#title').value;
+    const title = document.querySelector('#title-text').value;
     const description = document.querySelector('#description-text').value;
     const date = document.querySelector('#date-text').value;
-    var e = document.getElementById("ddlViewBy");
-    const priority = e.options[e.selectedIndex].text
-    const completion = document.querySelector('#read').checked;
+    const e = document.getElementById("priority-droplist");
+    const priority = e.options[e.selectedIndex].text;
+    const completion = false;
 
-    addBookToList(title, description, date, priority, completion);
+    addTaskToList(title, description, date, priority, completion);
   });
 }
+
+uploadTask();
+addTaskToList('Homework', 'Biology homework for miss Lily', '2018-05-31', 'High', true);
+addTaskToList('Homework', 'Biology homework for miss Lily', '2018-05-31', 'High', true);
