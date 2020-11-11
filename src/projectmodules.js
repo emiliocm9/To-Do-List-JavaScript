504761fd9952df53b3e597b3d01a1e03153b0612
emiliocm9 <emilio.contreras97@gmail.com>
2020-11-10 18:18:48 -0600

    docs: local storage projects

--- src/projectmodules.js
+++ src/projectmodules.js
@@ -18,9 +18,11 @@ const displayTasksOf = (listIndex, project) => {
 
 const displayProjects = (project) => {
   const listIndex = myProjects.indexOf(project);
+  const listade = localStorage.getItem(project.name);
+  const dataOfProject = JSON.parse(listade);
   const cont =  document.createElement('div');
   cont.setAttribute('data-index', `${listIndex}`);
-  cont.innerHTML += `<a href="#" data-text="${project.name}" class="title_menu title_bond">${project.name}</a>`;
+  cont.innerHTML += `<a href="#" data-text="${dataOfProject.name}" class="title_menu title_bond">${dataOfProject.name}</a>`;
 
   procontainer.appendChild(cont);
   displayTasksOf(listIndex, project);
@@ -30,19 +32,32 @@ const addProjectToList = (name, description, task) => {
   const newProject = new Project(name, description);
   newProject.addTasks(task);
   myProjects.push(newProject);
+  appendLocalStorage(name, newProject)
   displayProjects(newProject);
   dropProject(newProject);
-  appendLocalStorage(name, newProject)
 }
 
 const appendLocalStorage = (storage, item) => {
   localStorage.setItem(storage, JSON.stringify(item));
 }
 
+const initAppendLocalStorage = () => {
+  const one = new Project('Closure of Plant', 'Nationwide rail company KiwiRail chose to close an industrial plant that manufactured rolling stock and replacement parts.')
+  const two = new Project('Kitchen Makeover', 'Renovating the kitchen.');
+  const three = new Project('New Car Design', 'Designing a new transportation vehicle.');
+  localStorage.setItem('Closure of Plant', JSON.stringify(one));
+  localStorage.setItem('Kitchen Makeover', JSON.stringify(two));
+  localStorage.setItem('New Car Design', JSON.stringify(three));
+}
+
 const initFun = () => {
-  addProjectToList('Closure of Plant', 'Nationwide rail company KiwiRail chose to close an industrial plant that manufactured rolling stock and replacement parts.');
-  addProjectToList('Kitchen Makeover', 'Renovating the kitchen.');
-  addProjectToList('New Car Design', 'Designing a new transportation vehicle.');
+  initAppendLocalStorage();
+  for (let key in localStorage) {
+      if (key != 'length' && key != 'undefined') {
+      const content = JSON.parse(localStorage[key]);
+      addProjectToList(content.name, content.description);
+    }
+  }
   giveTasks();
   hideEditTask();
 }
