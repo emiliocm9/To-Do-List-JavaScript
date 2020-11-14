e4a00180c31ea4c089f63614c8fd1c8c84626907
emiliocm9 <emilio.contreras97@gmail.com>
2020-11-13 18:46:38 -0600

    test: task creation testing

---
+++ src/tests/taskmodule.test.js
@@ -0,0 +1,18 @@
+import { expect, test } from '@jest/globals';
+import { title } from 'process';
+import Task from '../taskmodule';
+
+test('Create new Task with correct information', () => {
+  const newTask = new Task('Biology Homework', 'Complete the test using your book', '10-29-2024', 'Low', false);
+
+  expect(newTask).toBeTruthy();
+  expect(newTask).toBeDefined();
+  expect(newTask).toEqual({title: 'Biology Homework', description: 'Complete the test using your book', date: '10-29-2024', priority: 'Low', completion: false})
+});
+
+test('Wrong task creation without title', () => {
+  const newTask = new Task(undefined, 'Complete the test using your book', '10-29-2024', 'Low', false);
+
+  expect(newTask.title).toBeUndefined();
+  expect(newTask).toEqual({title: undefined, description: 'Complete the test using your book', date: '10-29-2024', priority: 'Low', completion: false})
+});
